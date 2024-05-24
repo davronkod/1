91f02bfa7538c93d0f3949d77f1e27325da3d169
Davronbek <davroncoder074@gmail.com>
2024-05-24 13:06:13 +0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+const menuIconsEl = document.querySelector(".bi-list");
+const sidebarEl = document.querySelector(".sidebar");
+menuIconsEl.addEventListener("click", () => {
+    sidebarEl.style.marginTop = "0px"
+})
